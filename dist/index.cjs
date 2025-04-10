5c77535dd4ea992c6b139f85de421cde1492d270
Pedram Abdzadeh <abdzadehpedram@gmail.com>
2025-04-10 15:14:55 +0330

    Revert "refactor: some improvements and make project ready for release"
    This reverts commit c94e650cf0a81072f821e65116932cde1a56c675.

--- dist/index.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// src/index.ts
-var src_default = {};
-
-module.exports = src_default;
-//# sourceMappingURL=index.cjs.map
-//# sourceMappingURL=index.cjs.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// src/index.ts
-var src_default = {};
-
-export { src_default as default };
-//# sourceMappingURL=index.js.map
-//# sourceMappingURL=index.js.map
